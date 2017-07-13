dffbd80a1f25bd69ef79c8f730577ba82f69e280
tmptrash <deadbrainman@gmail.com>
2017-07-13 08:30:32 +0300

    tests for Code class

--- jasmine/spec/organism/CodeSpec.js
+++ jasmine/spec/organism/CodeSpec.js
@@ -40,17 +40,50 @@ describe("src/organism/Code", () => {
         code.destroy();
     });
 
-    it("Checking clone", () => {
-        let code  = new Code((()=>{}));
-        let code1 = new Code(()=>{});
+    it("Checking clone()", () => {
+        let code1 = new Code((()=>{}));
+        let code2 = new Code(()=>{});
+        let bc1;
+        let bc2;
 
-        code.insertLine();
-        expect(code.size).toEqual(1);
-        code1.clone(code);
+        expect(code1.size).toEqual(0);
+        code1.insertLine();
         expect(code1.size).toEqual(1);
-        expect(code1._byteCode[0] === code._byteCode[0]).toEqual(true);
+        code2.clone(code1);
+        expect(code2.size).toEqual(1);
+        bc1 = code1.cloneByteCode();
+        bc2 = code2.cloneByteCode();
+        expect(bc1[0] === bc2[0]).toEqual(true);
 
-        code.destroy();
+        code1.destroy();
+    });
+
+    it("Checking cloneCode()", () => {
+        let code1 = new Code((()=>{}));
+        let bc1;
+
+        expect(code1.size).toEqual(0);
+        code1.insertLine();
+        expect(code1.size).toEqual(1);
+        bc1 = code1.cloneCode();
+        expect(code1._code !== bc1).toEqual(true);
+        expect(bc1[0] === code1._code[0]).toEqual(true);
+
+        code1.destroy();
+    });
+
+    it("Checking cloneByteCode()", () => {
+        let code1 = new Code((()=>{}));
+        let bc1;
+
+        expect(code1.size).toEqual(0);
+        code1.insertLine();
+        expect(code1.size).toEqual(1);
+        bc1 = code1.cloneByteCode();
+        expect(code1._byteCode !== bc1).toEqual(true);
+        expect(bc1[0] === code1._byteCode[0]).toEqual(true);
+
+        code1.destroy();
     });
 
     it('Checking insertLine() method', () => {
