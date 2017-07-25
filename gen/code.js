e51ffe8a32bc2a26d07ccb013418f79fc7ea5d56
tmptrash <deadbrainman@gmail.com>
2017-07-25 23:46:38 +0300

    added generated code

---
+++ gen/code.js
@@ -0,0 +1,428 @@
+v3 = org.toMem(v0)
+v0 = 60024
+v2 = org.eatLeft(v3)
+v3 = org.lookAt(v3, v3)
+v2 = org.stepUp()
+v0 = org.fromMem()
+for (v2 = v3; v2 < v1; v2++) {
+    v3 = 491852
+    v1 = org.stepLeft()
+    v2 = org.eatUp(v3)
+    v1 = org.stepUp()
+    v2 = org.eatLeft(v3)
+    v0 = org.eatLeft(v2)
+    for (v1 = v3; v1 < v0; v1++) {
+        v2 = v1 >>> v1
+        v3 = org.stepUp()
+        if (v3 > v3) {
+            v3 = org.fromMem()
+            v1 = org.lookAt(v1, v1)
+        }
+        v1 = org.stepUp()
+        v1 = org.stepLeft()
+        v3 = org.eatDown(v1)
+        v0 = 754900
+        v1 = org.fromMem()
+        v1 = org.myY()
+        v0 = org.eatDown(v0)
+        v3 = org.eatDown(v2)
+    }
+    v1 = org.stepUp()
+    v3 = org.stepUp()
+    if (v2 == v1) {
+        v3 = Math.cos(v0)
+        v3 = org.toMem(v2)
+        v1 = 443357
+        v2 = v3 ^ v0
+        v3 = v0
+        v1 = org.eatDown(v2)
+        v1 = org.stepDown()
+        v1 = org.toMem(v2)
+        v0 = org.myY()
+        if (v3 < v2) {
+            v2 = org.lookAt(v3, v0)
+            v2 = org.stepUp()
+            v3 = 1042716
+            v3 = org.stepRight()
+            v3 = org.eatUp(v3)
+            v2 = +!v2
+            for (v3 = v0; v3 < v1; v3++) {}
+            v2 = v3 | v3
+        }
+        if (v2 < v2) {
+            v2 = org.stepUp()
+            if (v2 > v1) {
+                v0 = org.eatUp(v1)
+                v3 = org.fromMem()
+                v1 = org.stepUp()
+                v1 = org.stepLeft()
+                v0 = org.eatLeft(v2)
+                v1 = org.stepLeft()
+                v1 = org.stepLeft()
+            }
+            v3 = v0
+            v3 = org.toMem(v0)
+            for (v1 = v2; v1 < v0; v1++) {
+                if (v2 > v2) {
+                    v0 = org.eatLeft(v3)
+                }
+                v0 = org.stepLeft()
+                v0 = org.stepDown()
+                if (v2 == v0) {
+                    v1 = org.stepRight()
+                    v2 = org.stepDown()
+                    v0 = Math.abs(v2)
+                    v1 = org.toMem(v2)
+                    v0 = 72196
+                    v2 = org.lookAt(v1, v0)
+                    v0 = org.stepDown()
+                }
+                v3 = org.myX()
+            }
+            v1 = Math.PI
+            v3 = Math.PI
+            for (v2 = v0; v2 < v0; v2++) {
+                if (v3 > v1) {
+                    v3 = org.stepDown()
+                    v2 = org.fromMem()
+                    v1 = org.stepLeft()
+                    v2 = org.lookAt(v1, v2)
+                    v0 = org.myY()
+                    v0 = org.eatUp(v3)
+                    v3 = org.eatRight(v2)
+                    v1 = org.lookAt(v3, v2)
+                    v3 = Math.PI
+                    v2 = Math.PI
+                }
+            }
+        }
+    }
+}
+
+
+
+
+v3 = org.toMem(v0)
+v0 = +!v1
+for (v2 = v1; v2 < v0; v2++) {
+    v3 = v0
+    v0 = org.toMem(v2)
+    v2 = org.eatLeft(v3)
+    v2 = org.stepUp()
+    v2 = Math.PI
+    for (v2 = v0; v2 < v1; v2++) {
+        v1 = org.eatDown(v0)
+        v3 = org.stepUp()
+        v1 = org.stepUp()
+        v3 = 804085
+        v3 = org.stepDown()
+        v3 = org.toMem(v3)
+        v0 = org.stepLeft()
+        if (v0 < v0) {
+            v3 = org.stepRight()
+            v0 = org.eatDown(v0)
+            v0 = org.eatUp(v2)
+            v0 = org.lookAt(v2, v3)
+            v0 = org.eatUp(v3)
+            v2 = org.eatRight(v1)
+            v0 = +!v3
+            v0 = org.stepLeft()
+            v1 = +!v0
+            v1 = org.stepDown()
+        }
+        if (v3 < v2) {
+            v2 = org.lookAt(v1, v2)
+            v2 = Math.tan(v1)
+            if (v3 < v3) {
+                v0 = Math.abs(v3)
+                v3 = org.myY()
+                v0 = org.myY()
+                v1 = org.stepUp()
+                if (v2 == v2) {
+                    v2 = org.eatLeft(v3)
+                    v0 = org.stepRight()
+                    v3 = 646481
+                    for (v1 = v3; v1 < v0; v1++) {
+                        v1 = org.stepDown()
+                        v3 = org.stepUp()
+                        v3 = org.fromMem()
+                        v2 = v0
+                        if (v1 < v2) {
+                            if (v2 < v2) {
+                                v2 = org.stepDown()
+                                if (v2 > v1) {
+                                    v0 = org.eatUp(v1)
+                                    v2 = Math.PI
+                                    v3 = org.fromMem()
+                                    v1 = org.myX()
+                                    v1 = org.stepLeft()
+                                    v0 = org.eatLeft(v2)
+                                    v1 = org.stepLeft()
+                                    v1 = org.stepLeft()
+                                    v3 = v0
+                                    v3 = 613805
+                                    v1 = 257555
+                                }
+                                v0 = org.eatLeft(v3)
+                                v0 = org.stepLeft()
+                                v2 = Math.tan(v2)
+                                if (v2 == v0) {
+                                    v1 = Math.PI
+                                    v2 = org.stepRight()
+                                    v1 = 680594
+                                    v2 = org.stepDown()
+                                    for (v3 = v2; v3 < v3; v3++) {
+                                        v1 = org.eatDown(v2)
+                                        v0 = Math.abs(v2)
+                                        v0 = org.eatUp(v3)
+                                        v0 = org.eatLeft(v1)
+                                        v0 = 687933
+                                        v3 = org.stepUp()
+                                        v2 = org.eatRight(v3)
+                                        if (v0 < v0) {
+                                            v3 = org.eatDown(v2)
+                                            v3 = Math.PI
+                                            v3 = org.eatUp(v1)
+                                        }
+                                        for (v2 = v0; v2 < v0; v2++) {
+                                            v0 = org.stepLeft()
+                                            v2 = org.stepUp()
+                                            v1 = org.lookAt(v2, v3)
+                                            if (v3 > v1) {
+                                                v3 = Math.PI
+                                                v1 = org.stepLeft()
+                                                v2 = org.eatRight(v1)
+                                                v3 = org.toMem(v2)
+                                                v0 = org.eatUp(v3)
+                                                v1 = org.lookAt(v3, v2)
+                                                v0 = org.lookAt(v1, v3)
+                                                v3 = Math.PI
+                                                v1 = Math.PI
+                                                v0 = org.eatRight(v1)
+                                                v2 = Math.PI
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+v1 = +!v2
+v3 = org.stepRight()
+v3 = org.eatUp(v2)
+for (v2 = v1; v2 < v0; v2++) {
+    v3 = v0
+    v0 = org.toMem(v2)
+    v2 = v2 / v2
+    v2 = Math.sin(v1)
+    v2 = org.myY()
+    v2 = org.stepUp()
+    if (v2 > v1) {
+        v0 = org.eatUp(v1)
+        v3 = org.fromMem()
+        v1 = org.myX()
+        v2 = org.stepLeft()
+        v0 = org.eatLeft(v2)
+        v1 = org.stepLeft()
+        v1 = org.stepLeft()
+    }
+    v0 = org.eatRight(v3)
+    v3 = 93763
+    v1 = 185038
+    v0 = org.eatLeft(v3)
+    v0 = org.stepLeft()
+    v0 = +!v3
+    if (v2 == v0) {
+        v1 = org.stepRight()
+        v1 = 593293
+        v2 = org.stepDown()
+        v0 = org.eatRight(v1)
+        v0 = Math.abs(v2)
+        v1 = org.toMem(v2)
+        v0 = 1025942
+    }
+    v2 = org.lookAt(v1, v0)
+    v3 = org.myX()
+    if (v0 < v0) {
+        v2 = org.toMem(v0)
+        v3 = Math.PI
+        v3 = org.eatUp(v1)
+    }
+    for (v2 = v0; v2 < v0; v2++) {
+        if (v3 > v1) {
+            v3 = org.stepDown()
+            v2 = org.fromMem()
+            v1 = org.stepLeft()
+            v2 = org.lookAt(v1, v2)
+            v3 = org.toMem(v2)
+            v0 = org.eatDown(v1)
+            v1 = v2 < v3
+            v0 = org.toMem(v0)
+            v0 = v2 & v2
+            v0 = org.stepLeft()
+            for (v1 = v3; v1 < v0; v1++) {
+                v1 = org.stepDown()
+                v2 = org.eatDown(v0)
+                v2 = org.stepRight()
+                v3 = org.stepUp()
+                for (v3 = v3; v3 < v1; v3++) {
+                    v2 = org.fromMem()
+                    v3 = org.eatUp(v2)
+                }
+                v3 = org.toMem(v1)
+                v1 = Math.PI
+                v1 = org.stepUp()
+                v1 = 843530
+                v2 = org.eatRight(v3)
+                v3 = org.stepLeft()
+            }
+            v3 = org.lookAt(v0, v2)
+            for (v0 = v0; v0 < v2; v0++) {
+                v2 = org.fromMem()
+            }
+            v1 = 38498
+            v1 = org.fromMem()
+            v3 = Math.PI
+            v0 = Math.PI
+            v0 = org.eatUp(v3)
+            v0 = org.stepLeft()
+            v0 = 370276
+            for (v3 = v2; v3 < v0; v3++) {
+                v1 = org.lookAt(v3, v2)
+                if (v0 < v0) {
+                    v3 = Math.abs(v0)
+                    v1 = org.toMem(v1)
+                    v3 = Math.PI
+                }
+                v3 = Math.PI
+                v1 = org.stepLeft()
+                v0 = org.stepRight()
+                v3 = org.toMem(v2)
+                v0 = org.eatUp(v3)
+                v1 = org.fromMem()
+                v3 = org.stepUp()
+                v1 = org.stepDown()
+                v1 = org.stepLeft()
+                v3 = org.myY()
+            }
+            v1 = Math.PI
+            v1 = Math.PI
+        }
+    }
+}
+
+
+
+
+v1 = +!v2
+v3 = org.stepRight()
+v3 = org.eatUp(v2)
+for (v2 = v1; v2 < v0; v2++) {
+    v3 = v0
+    v0 = org.toMem(v2)
+    v2 = v2 / v2
+    v2 = Math.sin(v1)
+    v2 = org.myY()
+    v2 = org.stepUp()
+    if (v2 > v1) {
+        v0 = org.eatUp(v1)
+        v3 = org.fromMem()
+        v1 = org.myX()
+        v2 = org.stepLeft()
+        v0 = org.eatLeft(v2)
+        v1 = org.stepLeft()
+        v1 = org.stepLeft()
+    }
+    v0 = org.eatRight(v3)
+    v3 = 622078
+    v1 = 167420
+    v0 = org.eatLeft(v3)
+    v0 = org.stepLeft()
+    v0 = +!v3
+    if (v2 == v0) {
+        v1 = org.stepRight()
+        v1 = 982814
+        v2 = org.stepDown()
+        v0 = org.eatRight(v1)
+        v0 = Math.abs(v2)
+        v1 = org.toMem(v2)
+        v0 = 84370
+    }
+    v2 = org.lookAt(v1, v0)
+    v3 = org.myX()
+    if (v0 < v0) {
+        v2 = org.toMem(v0)
+        v3 = Math.PI
+        v3 = org.eatUp(v1)
+    }
+    for (v2 = v0; v2 < v0; v2++) {
+        if (v3 > v1) {
+            v3 = org.stepDown()
+            v2 = org.fromMem()
+            v1 = org.stepLeft()
+            v2 = org.lookAt(v1, v2)
+            v3 = org.toMem(v2)
+            v0 = org.eatDown(v1)
+            v1 = v2 < v3
+            v0 = org.toMem(v0)
+            v0 = v2 & v2
+            v0 = org.stepLeft()
+            for (v1 = v3; v1 < v0; v1++) {
+                v1 = org.stepDown()
+                v2 = org.eatDown(v0)
+                v2 = org.stepRight()
+                v3 = org.stepUp()
+                for (v3 = v3; v3 < v1; v3++) {
+                    v2 = org.fromMem()
+                    v3 = org.eatUp(v2)
+                }
+                v3 = org.toMem(v1)
+                v1 = Math.PI
+                v1 = org.stepUp()
+                v1 = 453271
+                v2 = org.eatRight(v3)
+                v3 = org.stepLeft()
+            }
+            v3 = org.lookAt(v0, v2)
+            for (v0 = v0; v0 < v2; v0++) {
+                v2 = org.fromMem()
+            }
+            v1 = 354559
+            v1 = org.fromMem()
+            v3 = Math.PI
+            v0 = Math.PI
+            v0 = org.eatUp(v3)
+            v0 = org.stepLeft()
+            v0 = 875755
+            for (v3 = v2; v3 < v0; v3++) {
+                v1 = org.lookAt(v3, v2)
+                if (v0 < v0) {
+                    v3 = Math.abs(v0)
+                    v1 = org.toMem(v1)
+                    v3 = Math.PI
+                }
+                v3 = Math.PI
+                v1 = org.stepLeft()
+                v0 = org.stepRight()
+                v3 = org.toMem(v2)
+                v0 = org.eatUp(v3)
+                v1 = org.fromMem()
+                v3 = org.stepUp()
+                v1 = org.stepDown()
+                v1 = org.stepLeft()
+                v3 = org.myY()
+            }
+            v1 = Math.PI
+            v1 = Math.PI
+        }
+    }
+}
