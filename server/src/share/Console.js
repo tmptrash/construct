7e76bb97f21d70684c5f8143284263f1b0f92cb0
DeadbraiN <deadbrainman@gmail.com>
2017-12-06 18:29:48 +0200

    fixed #80

--- server/src/share/Console.js
+++ server/src/share/Console.js
@@ -4,9 +4,14 @@
  * @author flatline
  */
 class Console {
-    static error(...msg) {console.log('\x1b[31m%s\x1b[0m', msg.join(''))}
-    static warn (...msg) {console.log('\x1b[33m%s\x1b[0m', msg.join(''))}
-    static info (...msg) {console.log('\x1b[37m%s\x1b[0m', msg.join(''))}
+    static error(...msg) {console.log(this._time() + '\x1b[31m%s\x1b[0m', msg.join(''))}
+    static warn (...msg) {console.log(this._time() + '\x1b[33m%s\x1b[0m', msg.join(''))}
+    static info (...msg) {console.log(this._time() + '\x1b[37m%s\x1b[0m', msg.join(''))}
+    static _time() {
+        const date = new Date;
+
+        return `${date.toLocaleDateString()} ${date.toLocaleTimeString()} `;
+    }
 }
 
 module.exports = Console;
