38f48c8b638143dcbb5892803d3e4af0e60c9d1d
DeadbraiN <deadbrainman@gmail.com>
2017-12-31 13:52:20 +0200

    fixed small issue with status padding (iq parameter)

--- client/src/manager/plugins/status/Status.js
+++ client/src/manager/plugins/status/Status.js
@@ -75,7 +75,7 @@ class Status extends Configurable {
         const slps      = format(this._runLines / this._times, 'lps', orgAmount, 0, 14         );
         const sorgs     = format(orgAmount,                    'org', orgAmount, 0, 10         );
         const senergy   = format(this._curEnergy,              'nrg', orgAmount, 0, 14         );
-        const siq       = format(this._energy,                 'iq',  orgAmount, 3, 13, 100000 );
+        const siq       = format(this._energy,                 'iq',  orgAmount, 3, 14, 100000 );
         const sfit      = format(this._fitness,                'fit', orgAmount, 3, 14         );
         const schanges  = format(this._changes,                'che', orgAmount, 3, 12, 1, true);
         const scode     = format(this._codeSize,               'cod', orgAmount, 1, 12, 1, true);
