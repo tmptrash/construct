72ef9d83dbd21f31e0ead7329fd5895744db9430
DeadbraiN <deadbrainman@gmail.com>
2018-02-20 21:55:21 +0200

    removed storing energy after death, because it's created a problem of new energy generation during moving between worlds/clients

--- client/src/manager/plugins/organisms/dos/OrganismSpec.js
+++ client/src/manager/plugins/organisms/dos/OrganismSpec.js
@@ -6,77 +6,63 @@ describe("client/src/organism/OrganismDos", () => {
     const OrganismDos = require('./Organism');
     const OConfig     = require('./../../../../manager/plugins/organisms/Config');
 
-    it("Checking organism creation", () => {
-        let org = new OrganismDos('0', 1, 2, true, null);
+    describe('Organism creation', () => {
+        it("Checking organism creation", () => {
+            let org = new OrganismDos('0', 1, 2, true, null);
 
-        expect(org.id).toEqual('0');
-        expect(org.x).toEqual(1);
-        expect(org.y).toEqual(2);
-        expect(org.item).toEqual(null);
-        expect(org.alive).toEqual(true);
-        expect(org.mutationProbs).toEqual(OConfig.orgMutationProbs);
-        expect(org.mutationProbs !== OConfig.orgMutationProbs).toEqual(true);
-        expect(org.mutationPeriod === OConfig.orgRainMutationPeriod).toEqual(true);
-        expect(org.mutationPercent === OConfig.orgRainMutationPercent).toEqual(true);
-        expect(org.mem.length).toBe(Math.pow(2, OConfig.orgMemBits));
-        expect(org.mem).toEqual(_fill(new Array(Math.pow(2, OConfig.orgMemBits)), 0));
-        expect(org.changes).toBe(0);
-        expect(org.energy).toBe(OConfig.orgStartEnergy);
-        expect(org.iterations).toBe(-1);
+            expect(org.id).toEqual('0');
+            expect(org.x).toEqual(1);
+            expect(org.y).toEqual(2);
+            expect(org.item).toEqual(null);
+            expect(org.alive).toEqual(true);
+            expect(org.mutationProbs).toEqual(OConfig.orgMutationProbs);
+            expect(org.mutationProbs !== OConfig.orgMutationProbs).toEqual(true);
+            expect(org.mutationPeriod === OConfig.orgRainMutationPeriod).toEqual(true);
+            expect(org.mutationPercent === OConfig.orgRainMutationPercent).toEqual(true);
+            expect(org.mem.length).toBe(Math.pow(2, OConfig.orgMemBits));
+            expect(org.mem).toEqual(_fill(new Array(Math.pow(2, OConfig.orgMemBits)), 0));
+            expect(org.changes).toBe(0);
+            expect(org.energy).toBe(OConfig.orgStartEnergy);
+            expect(org.iterations).toBe(-1);
 
-        org.destroy();
-    });
+            org.destroy();
+        });
 
-    it("Checking organism creation from parent", () => {
-        const memSize = OConfig.orgMemBits;
-        const parent  = new OrganismDos('1', 3, 4, true, null);
+        it("Checking organism creation from parent", () => {
+            const memSize = OConfig.orgMemBits;
+            const parent  = new OrganismDos('1', 3, 4, true, null);
 
-        OConfig.orgMemBits = 2;
-        parent.vm.insertLine();
-        parent.energy               = 123;
-        parent.changes              = 0xaabbcc;
-        parent.mutationProbs.splice(0, parent.mutationProbs.length, ...[5,8,1,10,1,2,32,7]);
-        parent.mutationPeriod       = 145;
-        parent.mutationPercent      = 0.2;
-        parent.mem.splice(0, parent.mem.length, ...[1,2,4,3]);
+            OConfig.orgMemBits = 2;
+            parent.vm.insertLine();
+            parent.energy               = 123;
+            parent.changes              = 0xaabbcc;
+            parent.mutationProbs.splice(0, parent.mutationProbs.length, ...[5,8,1,10,1,2,32,7]);
+            parent.mutationPeriod       = 145;
+            parent.mutationPercent      = 0.2;
+            parent.mem.splice(0, parent.mem.length, ...[1,2,4,3]);
 
-        let org = new OrganismDos('0', 1, 2, true, null, parent);
+            let org = new OrganismDos('0', 1, 2, true, null, parent);
 
-        expect(org.vm.code).toEqual(parent.vm.code);
-        expect(org.vm.size).toEqual(parent.vm.size);
-        expect(org.energy).toEqual(parent.energy);
-        expect(org.color).toEqual(parent.color);
-        expect(org.mutationProbs).toEqual(parent.mutationProbs);
-        expect(org.mutationPeriod).toEqual(parent.mutationPeriod);
-        expect(org.mutationPercent).toEqual(parent.mutationPercent);
-        expect(org.mem).toEqual(parent.mem);
-        expect(org.changes).toEqual(0);
-        expect(org.iterations).toEqual(-1);
-        expect(org.alive).toEqual(true);
-        expect(org.item).toEqual(null);
+            expect(org.vm.code).toEqual(parent.vm.code);
+            expect(org.vm.size).toEqual(parent.vm.size);
+            expect(org.energy).toEqual(parent.energy);
+            expect(org.color).toEqual(parent.color);
+            expect(org.mutationProbs).toEqual(parent.mutationProbs);
+            expect(org.mutationPeriod).toEqual(parent.mutationPeriod);
+            expect(org.mutationPercent).toEqual(parent.mutationPercent);
+            expect(org.mem).toEqual(parent.mem);
+            expect(org.changes).toEqual(0);
+            expect(org.iterations).toEqual(-1);
+            expect(org.alive).toEqual(true);
+            expect(org.item).toEqual(null);
 
-        org.destroy();
-        OConfig.orgMemBits = memSize;
-    });
-
-    it("Checking organism coordinates", () => {
-        let   org  = new OrganismDos(0, 1, 2, true, null, () => {});
-
-        org.x = 4;
-        org.y = 5;
-        expect(org.x === 4 && org.y === 5).toEqual(true);
-        org.x = 0;
-        org.y = 0;
-        expect(org.x === 0 && org.y === 0).toEqual(true);
-        org.x = -1;
-        org.y = -2;
-        expect(org.x === -1 && org.y === -2).toEqual(true);
-
-        org.destroy();
+            org.destroy();
+            OConfig.orgMemBits = memSize;
+        });
     });
 
     it("Checking if organism if alive", () => {
-        let   org     = new OrganismDos(0, 1, 2, true, null, () => {});
+        let   org     = new OrganismDos('0', 1, 2, true, null);
         const period  = OConfig.orgAlivePeriod;
         const energy  = OConfig.orgStartEnergy;
         const speriod = OConfig.orgEnergySpendPeriod;
